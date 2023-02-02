7eb63b4f0d30d38ca5612cecd46a6c80287b94e5
Magdum shaikh <94826076+smagdum124@users.noreply.github.com>
2023-02-02 16:52:23 +0530

    first commit

---
+++ oddEvenNumber.js
@@ -0,0 +1,42 @@
+// for (var a = 1; a <= 10; a++) {
+//   if (a % 2 == 0) {
+//     console.log(a);
+//   }
+// }
+
+// find the even number
+// for (var a = 1; a <= 10; a++) {
+//   if (a % 2 != 0) {
+//     console.log(a);
+//   }
+// }
+
+// find adult age example
+// var age = [10, 10, 10, 10, 10];
+
+// var adult=age.filter(function(){
+//   return age>=18;
+// })
+
+// console.log(adult)
+
+// var ,let ,const
+// [1]var variable= we can reassign and also redeclared in var variable and var varialble is globally accessiable
+// var a="hello";
+// var a="hello world";
+// a="hello magdum"
+// console.log(a);
+
+// [2] let := we canot reasign but we can redeclared and let is block scope
+
+// let a="hello";
+// let a="hello world";
+// a="hello magdum"
+// console.log(a);
+
+// [3]const ;= we can't reasign and  can't redeclare
+
+// const a="hello";
+// const a="magdum"
+// a="magdum"
+// console.log(a);
